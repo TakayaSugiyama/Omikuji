c2cd82af881c60c48c7aa56a29bebfa57371dec7
takayasugiyama <s.takaya1027@gmail.com>
2020-06-05 05:01:16 +0900

    init

---
+++ app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiActivity.kt
@@ -0,0 +1,11 @@
+package jp.wings.nikkeibp.omikuji
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OmikujiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
