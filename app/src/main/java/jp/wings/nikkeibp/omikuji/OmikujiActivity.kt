7ccf8772504817b5059b3bcdd7002e7f4d381943
takayasugiyama <s.takaya1027@gmail.com>
2021-08-11 18:04:11 +0900

    :+1: if-else文を学んだ。

--- app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiActivity.kt
+++ app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiActivity.kt
@@ -3,11 +3,21 @@ package jp.wings.nikkeibp.omikuji
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.main.*
+import java.util.*
 
 class OmikujiActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
-        hello_view.text = "おみくじアプリ"
+        //文字を表示する。
+        var str = "大吉"
+        val rnd = Random()
+        val number = rnd.nextInt(3)
+        if (number == 0){
+            str = "吉"
+        }else if (number == 1){
+            str = "凶"
+        }
+        hello_view.text = str
     }
 }
