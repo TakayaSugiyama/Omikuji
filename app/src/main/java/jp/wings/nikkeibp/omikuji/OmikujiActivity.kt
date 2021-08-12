30af7ccd29d5ca4f027a5412e03afc212d91e23a
takayasugiyama <s.takaya1027@gmail.com>
2021-08-12 20:32:34 +0900

    add image

--- app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiActivity.kt
+++ app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiActivity.kt
@@ -3,23 +3,25 @@ package jp.wings.nikkeibp.omikuji
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.main.*
+import kotlinx.android.synthetic.main.omikuji.*
 import java.util.*
 
 class OmikujiActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main)
-        //くじ番号の取得
-        val rnd = Random()
-        val number = rnd.nextInt(20)
-
-        //おみくじの準備
-        val omikujiShelf = Array(20) { "吉"}
-        omikujiShelf[0] = "大吉"
-        omikujiShelf[19] = "凶"
-
-        //おみくじ棚から取得
-        val str = omikujiShelf[number]
-        hello_view.text = str
+        setContentView(R.layout.omikuji)
+        imageView.setImageResource(R.drawable.result1)
+//        //くじ番号の取得
+//        val rnd = Random()
+//        val number = rnd.nextInt(20)
+//
+//        //おみくじの準備
+//        val omikujiShelf = Array(20) { "吉"}
+//        omikujiShelf[0] = "大吉"
+//        omikujiShelf[19] = "凶"
+//
+//        //おみくじ棚から取得
+//        val str: String = omikujiShelf[number]
+//        hello_view.text = str
     }
 }
