593fbea8fa76aea86cd649e94def41cddcfd4465
takayasugiyama <s.takaya1027@gmail.com>
2021-08-12 18:13:24 +0900

    :+1: learn array

--- app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiActivity.kt
+++ app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiActivity.kt
@@ -9,15 +9,17 @@ class OmikujiActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
-        //文字を表示する。
-        var str = "大吉"
+        //くじ番号の取得
         val rnd = Random()
-        val number = rnd.nextInt(3)
-        if (number == 0){
-            str = "吉"
-        }else if (number == 1){
-            str = "凶"
-        }
+        val number = rnd.nextInt(20)
+
+        //おみくじの準備
+        val omikujiShelf = Array(20) { "吉"}
+        omikujiShelf[0] = "大吉"
+        omikujiShelf[19] = "凶"
+
+        //おみくじ棚から取得
+        val str = omikujiShelf[number]
         hello_view.text = str
     }
 }
