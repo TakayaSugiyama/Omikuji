c3a2cad8d101c1f66e0f66240ed9bc40bb2a672b
takayasugiyama <s.takaya1027@gmail.com>
2021-08-14 10:40:02 +0900

    :+1: Add Animation

--- app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiActivity.kt
+++ app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiActivity.kt
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import java.util.*
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.RotateAnimation
+import android.view.animation.TranslateAnimation
 import kotlinx.android.synthetic.main.omikuji.*
+import kotlin.math.round
 
 class OmikujiActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +31,19 @@ class OmikujiActivity : AppCompatActivity() {
     }
 
     fun onButtonClick(view: View){
-        imageView.setImageResource(R.drawable.result1)
+        //imageView.setImageResource(R.drawable.result1)
+        val translate = TranslateAnimation(0f, 0f, 0f, -200f)
+        translate.repeatMode = Animation.REVERSE
+        translate.repeatCount = 10
+        translate.duration = 100
+
+        val rotate = RotateAnimation(0f, -36f, imageView.width/2f, imageView.height/2f)
+        rotate.duration = 200
+
+        val set = AnimationSet(true)
+        set.addAnimation(rotate)
+        set.addAnimation(translate)
+
+        imageView.startAnimation(set)
     }
 }
